24bd85ac915076aea677491f7f50b78c82fcf45b
Victor Nogueira <ovictorhnogueira@gmail.com>
2020-12-07 23:57:51 +0000

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,8 @@ export default function Home() {
             }
             {darkMode === 'light' && (
               <button
-                className="js-change-theme focus:outline-none text-gray-500 p-2 bg-gray-200 rounded-full"
+                className="focus:outline-none text-gray-500 p-2 bg-gray-200 rounded-full"
+                aria-label="Alterar tema"
                 onClick={() => setDarkmode('dark')}
               >
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg>
